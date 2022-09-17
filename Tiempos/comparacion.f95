912b8b5e4c2ab1946a72d33edb6cb10ad360b94d
DSarceno <dsarceno68@gmail.com>
2022-09-16 23:25:25 -0600

    tiempos

---
+++ Tiempos/comparacion.f95
@@ -0,0 +1,16 @@
+! vie 16 sep 2022 19:52:40 CST 
+! comparacion.f95 
+! Diego Sarceno (dsarceno68@gmail.com) 
+
+! Resumen 
+
+! Codificado del texto: UTF8 
+! Compiladores probados: GNU Fortran (Ubuntu 20.04 Linux) 9.4.0 
+! Instruciones de Ejecución: no requiere nada mas 
+! gfortran -Wall -pedantic -std=f95 -c -o comparacion.o comparacion.f95 
+! gfortran -o comparacion.x comparacion.o 
+
+! PROGRAM comparacion 
+
+! END comparacion 
+
